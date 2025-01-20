73b15c8446904b23cb8d87edc1573d00389230d2
Igor Mirsalikhov <imirsalikhov7421@floridapoly.edu>
2025-01-20 13:22:12 -0500

    Implemented print_arrow

---
+++ print_arrow.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(void) {
+	int baseInt;
+	int headInt;
+
+	puts("Enter the number for arrow body:");
+	scanf("%d", &baseInt);
+	puts("Enter the number for arrow head:");
+	scanf("%d", &headInt);
+
+	printf("    %d\n", baseInt);
+	printf("    %d%d\n", headInt, headInt);
+	printf("%d%d%d%d%d%d%d\n", baseInt, baseInt, baseInt, baseInt, headInt, headInt, headInt);
+	printf("%d%d%d%d%d%d%d%d\n", baseInt, baseInt, baseInt, baseInt, headInt, headInt, headInt, headInt);
+	printf("%d%d%d%d%d%d%d\n", baseInt, baseInt, baseInt, baseInt, headInt, headInt, headInt);
+	printf("    %d%d\n", headInt, headInt);
+	printf("    %d\n", headInt);
+
+	return 0;
+}
